99608ded0c5e59d01fdb5cfe22fc44f3cc3c225c
Luan-Vn4 <luanvilaca1@gmail.com>
2024-06-21 11:55:52 -0300

    first commit

---
+++ src/main/java/br/upe/sap/sistemasapupe/SistemaSapUpeApplication.java
@@ -0,0 +1,13 @@
+package br.upe.sap.sistemasapupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSapUpeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaSapUpeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/upe/sap/sistemasapupe/SistemaSapUpeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.sap.sistemasapupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSapUpeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
