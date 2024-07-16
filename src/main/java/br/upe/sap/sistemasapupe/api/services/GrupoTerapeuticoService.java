f73a7da7f08c560f319f37826f4c1a2d380e6274
Pedro-RGS <fritz.hard@gmail.com>
2024-07-16 09:44:13 -0300

    'Criacao do DTO do grupo terapeutico e do Service'

--- src/main/java/br/upe/sap/sistemasapupe/api/services/GrupoTerapeuticoService.java
+++ src/main/java/br/upe/sap/sistemasapupe/api/services/GrupoTerapeuticoService.java
@@ -1,14 +1,12 @@
 package br.upe.sap.sistemasapupe.api.services;
 
 import br.upe.sap.sistemasapupe.data.repositories.interfaces.GrupoTerapeuticoRepository;
+import br.upe.sap.sistemasapupe.data.repositories.jdbi.JdbiGrupoTerapeuticoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
 public class GrupoTerapeuticoService {
-
-    GrupoTerapeuticoRepository grupoTerapeuticoRepository;
-
-    // vou fazer primeiro o DTO
+    JdbiGrupoTerapeuticoRepository grupoTerapeuticoRepository;
 }
