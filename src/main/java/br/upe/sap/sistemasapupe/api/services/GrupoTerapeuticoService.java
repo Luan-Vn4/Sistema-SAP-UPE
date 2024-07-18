e33c66436ce6dcf01bbdbc4237e33eea3bb3acd1
Pedro-RGS <fritz.hard@gmail.com>
2024-07-18 17:24:36 -0300

    'Criação dos DTOs e começo do desenvolvimento do service'

--- src/main/java/br/upe/sap/sistemasapupe/api/services/GrupoTerapeuticoService.java
+++ src/main/java/br/upe/sap/sistemasapupe/api/services/GrupoTerapeuticoService.java
@@ -10,10 +10,6 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
-
-
 @AllArgsConstructor
 @Service
 public class GrupoTerapeuticoService {
