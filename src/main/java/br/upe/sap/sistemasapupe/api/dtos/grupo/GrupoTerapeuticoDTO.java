d0fa93f5043a7bb5c59f72eebb35d1c6557fdd2f
Pedro-RGS <fritz.hard@gmail.com>
2024-07-21 13:55:16 -0300

    'REdesenvolvimento do GrupoTerapeuticoService'

--- src/main/java/br/upe/sap/sistemasapupe/api/dtos/grupo/GrupoTerapeuticoDTO.java
+++ src/main/java/br/upe/sap/sistemasapupe/api/dtos/grupo/GrupoTerapeuticoDTO.java
@@ -3,11 +3,19 @@ package br.upe.sap.sistemasapupe.api.dtos.grupo;
 import br.upe.sap.sistemasapupe.api.dtos.paciente.FichaDTO;
 import br.upe.sap.sistemasapupe.api.dtos.funcionarios.FuncionarioDTO;
 import br.upe.sap.sistemasapupe.data.model.grupos.GrupoTerapeutico;
+import br.upe.sap.sistemasapupe.data.repositories.interfaces.FuncionarioRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 public record GrupoTerapeuticoDTO(UUID uid, String tema,
-                                  List<FuncionarioDTO> coordenador, List<FichaDTO> fichas) {
+                                  String descricao, UUID idDono) {
 
+    public static GrupoTerapeuticoDTO from (GrupoTerapeutico grupoTerapeutico,
+                                     FuncionarioRepository repository){
+        UUID uidDono = repository.findById(grupoTerapeutico.getIdDono()).getUid();
+
+        return new GrupoTerapeuticoDTO(grupoTerapeutico.getUid(), grupoTerapeutico.getTema(),
+                grupoTerapeutico.getDescricao(), uidDono);
+    }
 }
